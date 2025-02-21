8b0ba8f76c0c50e46cef9cd624352b49a27b2f3c
Lavianzy <shevaihsan5@gmail.com>
2025-02-21 18:07:28 +0700

    first commit latihan 6

---
+++ latihan_6-Komparasi/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a;
+    int b;
+
+    a = 5;
+    b = 10;
+
+    // menghasilkan output berupa boolean.
+    // 1 = true. 0 = false
+
+    // lebih kecil dari
+    cout << (a < b) << endl;
+
+    // lebih besar dari
+    cout << (a > b) << endl;
+
+    // sama dengan dari
+    cout << (a == b) << endl;
+
+    // tidak sama dengan dari
+    cout << (a != b) << endl;
+
+    // lebih kecil atau sama dengan dari
+    cout << (a <= b) << endl;
+
+    // lebih besar atau sama dengan dari
+    cout << (a >= b) << endl;
+
+    return 0;
+}
